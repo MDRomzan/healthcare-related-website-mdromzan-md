dd116ee7f63b5ce0b99468f4afac01e964f478f1
MDRomzan <mdromjan483@gmail.com>
2021-10-19 23:18:34 +0600

    yeah all

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -3,12 +3,15 @@ import Banner from '../Banner/Banner';
 import Doctors from '../Doctors/Doctors';
 import Extra from '../Extra/Extra';
 import HomeServices from '../HomeServices/HomeServices';
+import Idea from '../IDEA/Idea';
 
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <h1 className="text-center text ">Our Service Machince</h1>
+            <Idea></Idea>
             <HomeServices></HomeServices>
             <Extra></Extra>
             <Doctors></Doctors>
---
+++ src/pages/Home/IDEA/Idea.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Card, CardGroup } from 'react-bootstrap';
+import pic1 from "../../../images/pic1.jpeg"
+import pic2 from "../../../images/pic2.jpeg"
+import pic3 from "../../../images/pic3.jpeg"
+
+const Idea = () => {
+    return (
+        
+       <CardGroup>
+       
+  <Card>
+    <Card.Img variant="top" src={pic1} />
+    <Card.Body>
+      <Card.Title>Neatual Maehies</Card.Title>
+      <Card.Text>
+          High Quality Medical Instrument with Competitive Price.China’ s 
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+   
+  </Card>
+  <Card>
+    <Card.Img variant="top" src={pic2} />
+    <Card.Body>
+      <Card.Title>UltraSound Machines</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural technology, Solutions
+        for various oxygen concentrators.No.1 Korean manufacture
+        for Medical oxygen concentrator. {
+            ' '
+        }
+      </Card.Text>
+    </Card.Body>
+    
+  </Card>
+  <Card>
+    <Card.Img variant="top" src={pic3}/>
+    <Card.Body>
+      <Card.Title>Labortory Maehines</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    
+  </Card>
+</CardGroup>
+    );
+};
+
+export default Idea;
